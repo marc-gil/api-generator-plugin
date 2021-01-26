ed6ef0d90bef6149a9e8eb0441306a952fc339e5
GitHub Action <action@github.com>
2021-01-26 15:55:13 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/marcgil/apigeneratorplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.marcgil.apigeneratorplugin.services
+
+import com.github.marcgil.apigeneratorplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
